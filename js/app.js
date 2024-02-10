ae988940165d4a06b70db4f775c97ac4f68894fe
Rahul kumar Pandey <pandeycoder8474@gmail.com>
2024-02-10 06:05:34 +0530

    update next chapter

---
+++ js/app.js
@@ -0,0 +1,40 @@
+//console.log("hello world");
+//console.log("Rahul IT");
+//console.log("hello world");
+/*let a=10;
+let b=20;
+console.log("sum is : ",(a+b));
+let pecilPrice=10;
+let eraser=5;
+let output1="The price is "+(pecilPrice+eraser)+ " Rupess .";
+
+let output=`The total price is : ${eraser+pecilPrice} Rupess`;  //back ticks
+
+console.log(output1);*/
+//console.log("the total price is :",(a+b)," Rupess");
+
+
+
+            //Print console alert prompt
+
+/*alert("this is simple alert");
+
+
+console.log("this is simple log");
+console.error("this is simple error message");
+console.warn("this is warning message");
+console.info("this is info message");
+console.debug("this is debug message");
+console.trace("this is simple trace message");
+
+
+let first=prompt("enter your name");
+console.log(first);*/
+
+
+//let a="  hello  so   me     ";
+//a.trim();
+
+let ps=prompt("set your password");
+let newps=ps.trim();
+console.log(newps);
