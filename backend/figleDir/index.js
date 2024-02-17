92fb83f2794b8fc7b9aec503c7ab4fddb57a8f98
pandeycoder <pandeycoder8474@gmail.com>
2024-02-17 12:20:43 +0530

    add notes package and code

---
+++ backend/figleDir/index.js
@@ -0,0 +1,10 @@
+const figlet=require("figlet"); //package
+
+figlet("Pandeycoder!!", function (err, data) {
+    if (err) {
+      console.log("Something went wrong...");
+      console.dir(err);
+      return;
+    }
+    console.log(data);
+  });
---
+++ backend/script.js
@@ -0,0 +1,50 @@
+// let n=5;
+
+// for(let i=0;i<=n;i++)
+// {
+//     console.log("hello Rahul IT",i);
+// }
+// console.log("bye !");
+
+// let args=process.argv;
+
+// for(let i=0;i<args.length;i++)
+// {
+//     console.log("hello & welcome",args[i]);
+
+// }
+
+
+// const math=require("./math");  // import another file
+
+// console.log(math.sum(4,7));
+// console.log(math.pi);
+
+// const fruits = require("./Fruits");
+// console.log(fruits[0].name);
+
+
+ 
+// --------------import--------
+
+
+import {sum,pi} from "./math.js";
+
+console.log(sum(4,5));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
