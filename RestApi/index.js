ea753260ede813388a3d2aa9cb38018cf717d0c4
pandeycoder <pandeycoder8474@gmail.com>
2024-02-19 14:57:44 +0530

    add code

--- RestApi/index.js
+++ RestApi/index.js
@@ -5,10 +5,13 @@ const path=require("path");
 
 const {v4:uuidv4}=require('uuid'); //it create unique id 
 
+const methodOverride=require("method-override")
+
 
 app.use(express.urlencoded({extended:true}));
 app.use(express.json({extended:true}));
 
+app.use(methodOverride('_method'));
 
 app.set("view engine","ejs");
 app.use(express.static(path.join(__dirname,"/public")));
@@ -60,7 +63,6 @@ app.post("/posts",(req, res)=>{  //create the post
 
     posts.push({id,username,content});
 
-
     console.log(req.body);
 
     res.redirect("/posts");
@@ -78,13 +80,45 @@ app.get("/posts/:id",(req, res)=>{
     console.log(post);
 
     res.render("show.ejs",{post});
+});
+
+app.patch("/posts/:id",(req,res)=>{
+
+    let {id}=req.params;
+    console.log(id);
+
+    let  newContent=req.body.content;
+
+    let post=posts.find((p) => id == p.id );
+
+    
+    console.log(post);
+    
+    post.content=newContent;
+
+
+    res.redirect("/posts");
+
+});
+
+
+app.get("/posts/:id/edit",(req,res)=>{
+
+    
+    let {id}=req.params;
+    let post=posts.find((p) => id == p.id );
+    
+
+    res.render("edit.ejs",{post});
+});
+
+app.delete("/posts/:id",(req,res)=>{
+
+    let {id}=req.params;
+     posts=posts.filter((p) => id !== p.id );
+
+    res.redirect("/posts");
 })
-
-
-
-
-
-
 
 
 
